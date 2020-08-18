7918b10aba14b119e6654cff44ba75a35cf89e63
dkhayes117 <dkhayes117@yahoo.com>
2020-08-18 19:38:38 -0400

    Change binary format to make sure all 32 bits of the MISA register are printed

--- examples/read_extensions.rs
+++ examples/read_extensions.rs
@@ -20,11 +20,11 @@ fn main() -> ! {
     // Configure UART for stdout
     hifive1::stdout::configure(p.UART0, pin!(pins, uart0_tx), pin!(pins, uart0_rx), 115_200.bps(), clocks);
 
-    //Read the misa CSR to see all implemented extensions
+    //Read the misa CSR to see all implemented extensions and base ISA width
     let ext = riscv::register::misa::read();
     sprintln!("Preparing to read MISA register");
-    sprintln!("MXL  ZYXWVUTSRQPONMLKJIHGFEDCBA");
-    sprintln!("{:b}", ext.map_or(0, |v| v.bits()));
+    sprintln!("MXL   ZYXWVUTSRQPONMLKJIHGFEDCBA");
+    sprintln!("{:032b}", ext.map_or(0, |v| v.bits()));
 
     loop {}
 }
